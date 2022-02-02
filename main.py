fdc2892d289f6b76127cb8001d7127b5a9c82a51
Fabio Brasil <fabiobfelix@hotmail.com>
2022-02-01 23:39:06 -0300

    refatoração de relacionamento

--- main.py
+++ main.py
@@ -1,81 +1,14 @@
 
-from flask import Flask, request
+from urllib import response
+from flask import Flask, Response, request
 from flask_restful import Resource, Api
 from models import Colaboradores, Tarefas
 
 app = Flask(__name__)
 api = Api(app)
 
-class Colaborador(Resource):
-    def get(self,nome):
-        colaborador = Colaboradores.query.filter_by(nome=nome).first()
-        try:
-            response = {
-                'nome':colaborador.nome, 
-                'idade':colaborador.idade,
-                'email': colaborador.email,
-                'area_de_atuacao': colaborador.area_de_atuacao,
-                'tipo_de_contratacao': colaborador.tipo_contratacao,
-                'cargo': colaborador.cargo
-            }
-        except AttributeError:
-            response = {
-                'status': 'erro',
-                'message': 'Colaborador não Encontrado'
-            }
-
-        return response
-
-    def put(self, nome):
-        colaborador = Colaboradores.query.filter_by(nome=nome).first()
-        dados = request.json
-        if 'nome' in dados:
-            colaborador.nome = dados['nome']
-        if 'idade' in dados:
-            colaborador.idade = dados['idade']
-        if 'email' in dados:
-            colaborador.email = dados['email']
-        if 'area_de_atuacao' in dados:
-            colaborador.area_de_atuacao = dados['area_de_atuacao']
-        if 'tipo_contratacao' in dados:
-            colaborador.tipo_contratacao = dados['tipo_contratacao']
-        if 'cargo' in dados:
-            colaborador.cargo = dados['cargo']
-            
-        colaborador.save()
-
-        response = {'nome': colaborador.nome, 
-            'idade': colaborador.idade,
-            'email': colaborador.email,
-            'area_de_atuacao': colaborador.area_de_atuacao,
-            'tipo_contratacao': colaborador.tipo_contratacao,
-            'cargo': colaborador.cargo
-        }
-
-        return response 
-    
-    def delete(self, nome):
-        colaborador = Colaboradores.query.filter_by(nome=nome).first()
-        colaborador.delete()
-        mensagem = f'Colaborador {colaborador.nome} excluido do banco de dados.'
-
-        return {'status': 'Sucesso', 'messagem': mensagem}
-    
-class ListaColaboradores(Resource):
-    def get(self):
-        colaboradores = Colaboradores.query.all()
-        response = [{
-            'id': i.id,
-            'nome':i.nome,
-            'idade':i.idade,
-            'email': i.email,
-            'area_de_atuacao':i.area_de_atuacao,
-            'tipo_contratacao': i.tipo_contratacao,
-            'cargo': i.cargo
-        }  for i in colaboradores]
-        
-        return response
-    
+class CadastraColaborador(Resource):
+    # Cadastra um novo colaborador
     def post(self):
         dados = request.json
         colaborador = Colaboradores(
@@ -101,34 +34,190 @@ class ListaColaboradores(Resource):
 
         return response
 
+class VisuazilaColaborador(Resource):
+    # Lista colaborador pelo nome
+    def get(self,nome):
+        colaborador = Colaboradores.query.filter_by(nome=nome).first()
+        try:
+            response = {
+                'nome':colaborador.nome, 
+                'idade':colaborador.idade,
+                'email': colaborador.email,
+                'area_de_atuacao': colaborador.area_de_atuacao,
+                'tipo_de_contratacao': colaborador.tipo_contratacao,
+                'cargo': colaborador.cargo
+            }
+        except AttributeError:
+            response = {
+                'status': 'erro',
+                'message': 'Colaborador não Encontrado'
+            }
+
+        return response
+
+class ListaColaboradores(Resource):
+    # Lista todos os colaboradores cadastrados no banco de dados
+    def get(self):
+        colaboradores = Colaboradores.query.all()
+        response = [{
+            'id': i.id,
+            'nome':i.nome,
+            'idade':i.idade,
+            'email': i.email,
+            'area_de_atuacao':i.area_de_atuacao,
+            'tipo_contratacao': i.tipo_contratacao,
+            'cargo': i.cargo
+        }  for i in colaboradores]
+        
+        return response
+
+class AtualizaColaborador(Resource):
+    # Atualiza dados de um colaborador já cadastrado no banco de dados
+    def put(self, nome):
+        dados = request.json
+        colaborador = Colaboradores.query.filter_by(nome=nome).first()
+
+        if 'nome' in dados:
+            colaborador.nome = dados['nome']
+        if 'idade' in dados:
+            colaborador.idade = dados['idade']
+        if 'email' in dados:
+            colaborador.email = dados['email']
+        if 'area_de_atuacao' in dados:
+            colaborador.area_de_atuacao = dados['area_de_atuacao']
+        if 'tipo_contratacao' in dados:
+            colaborador.tipo_contratacao = dados['tipo_contratacao']
+        if 'cargo' in dados:
+            colaborador.cargo = dados['cargo']
+            
+        colaborador.save()
+
+        response = {'nome': colaborador.nome, 
+            'idade': colaborador.idade,
+            'email': colaborador.email,
+            'area_de_atuacao': colaborador.area_de_atuacao,
+            'tipo_contratacao': colaborador.tipo_contratacao,
+            'cargo': colaborador.cargo
+        }
+
+        return response 
+
+class RemoveColaborador(Resource):    
+    def delete(self, nome):
+        colaborador = Colaboradores.query.filter_by(nome=nome).first()
+        colaborador.delete()
+        mensagem = f'Colaborador {colaborador.nome} excluido do banco de dados.'
+
+        return {'status': 'Sucesso', 'messagem': mensagem}
+
+class criatarefa(Resource):
+    def post(self):
+        dados = request.json
+        tarefa = Tarefas(
+            id = dados['id'],
+            titulo_tarefa = dados['titulo_tarefa'],
+            descricao = dados['descricao'],
+            co_nome_fk = dados['co_nome_fk'],
+            solicitante_da_demanda = dados['solicitante_da_demanda'],  
+        )
+
+        tarefa.save()
+
+        response = {
+            'id': tarefa.id,
+            'titulo_tarefa': tarefa.titulo_tarefa,
+            'descricao': tarefa.descricao,
+            'co_nome_fk': tarefa.co_nome_fk,
+            'solicitante_da_demanda': tarefa.solicitante_da_demanda
+        }
+        return response
+
+
 class ListaTarefas(Resource):
     def get(self):
         tarefas = Tarefas.query.all()
         response = [{
-            'id':i.id, 
-            'tarefa': i.tarefa, 
-            'colaborador': i.colaborador.nome} 
-            for i in tarefas]
+            'id':i.id,
+            'titulo_tarefa': i.titulo_tarefa,
+            'descricao': i.descricao,
+            'co_nome_fk': i.colaborador.co_nome_fk, 
+            'solicitante_da_demanda': i.solicitante_da_demanda,
+            }
+            for i in tarefas
+        ]
     
         return response
 
-    def post(self): 
+class AtualizaTarefa:
+    def put(self,co_nome_fk ,titulo_tarefa):
         dados = request.json
-        colaborador = Colaboradores.query.filter_by(nome=dados['colaborador']).first()
-        tarefa = Tarefas(tarefa=dados['tarefa'], colaborador = colaborador)
+        co_nome_fk = Tarefas.query.filter_by(co_nome_fk=co_nome_fk).first()
+        tarefa = Tarefas.query.filter_by(titulo_tarefa=titulo_tarefa).first()
+
+        if not tarefa:
+            return Response("tarefa não encontrada", 404)        
+
+
+        if "co_nome_fk" in dados:
+            co_nome_fk.co_nome_fk = dados["co_nome_fk"]
+        if  "titulo_tarefa" in dados:
+            tarefa.titulo_tarefa = dados["titulo_tarefa"]
+        if "descricao" in dados:
+            tarefa.descricao = dados["descricao"]
+        if "solicitante_da_demanda" in dados:
+            tarefa.solicitante_da_demanda = dados["solicitante_da_demanda"]
+               
+
+        co_nome_fk.save()
         tarefa.save()
 
         response = {
-            'colaborador':tarefa.colaborador.nome,
-            'tarefa': tarefa.tarefa,
-            'id': tarefa.id
-        }
+            'id': tarefa.id,
+            'titulo_tarefa': tarefa.titulo_tarefa,
+            'descricao': tarefa.descricao,
+            'co_nome_fk':tarefa.co_nome_fk,
+            'solicitante_da_demanda': tarefa.solicitante_da_demanda
+            }
+        
+        return response   
 
-        return response
 
-api.add_resource(Colaborador, '/colaborador/<string:nome>/')
-api.add_resource(ListaColaboradores, '/lista_colaboradores/')
-api.add_resource(ListaTarefas, '/lista_tarefas/')
+  
+
+
+# def post(self): 
+#         dados = request.json
+#         colaborador = Colaboradores.query.filter_by(nome=dados['colaborador']).first()
+#         tarefa = Tarefas(tarefa=dados['tarefa'], colaborador = colaborador)
+#         tarefa.save()
+
+#         response = {
+#             'colaborador':tarefa.colaborador.nome,
+#             'tarefa': tarefa.tarefa,
+#             'id': tarefa.id
+#         }
+
+#         return response
+
+
+
+
+
+
+api.add_resource(CadastraColaborador,'/cadastar-colaborador/')
+api.add_resource(ListaColaboradores, '/lista-colaboradores/')
+api.add_resource(AtualizaColaborador, '/atualiza-colaborador/<string:nome>/')
+api.add_resource(VisuazilaColaborador, '/visuazila-colaborador/<string:nome>/')
+api.add_resource(RemoveColaborador, '/remove-colaborador/')
+api.add_resource(ListaTarefas, '/lista-tarefas/')
+
+
+
+# api.add_resource(Colaborador, '/colaborador/<string:nome>/')
+# api.add_resource(Tarefas, '/tarefas/')
+# api.add_resource(Tarefa, '/tarefas/<tarefa>')
+# api.add_resource(ColaboradorTarefa, '/tarefas/<colaborador>/<tarefa>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,28 +1,60 @@
-
+import sqlalchemy as sa
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine(r'sqlite:///colaboradores.db')
+engine = sa.create_engine(r'sqlite:///colaboradores.db')
 db_session = scoped_session(sessionmaker(autocommit=False, bind=engine))
 
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-class Colaboradores(Base):
-    __tablename__='colaboradores'
-    id = Column(Integer, primary_key=True)
-    nome = Column(String(40), index=True)
-    idade = Column(Integer)
-    email = Column(String(40))
-    area_de_atuacao = Column(String(10))
-    tipo_contratacao = Column(String(10))
-    cargo = Column(String(50))
-
+class Tarefas(Base):
+    __tablename__ = 'tb_tarefas'
+    id = sa.Column(Integer, primary_key=True, autoincrement=True)
+    titulo_tarefa = sa.Column(String(50))
+    descricao = sa.Column(sa.String(50))
+    co_nome_fk = sa.Column(sa.Integer, sa.ForeignKey("tb_colaboradores.id"))
+    # co_nome_fk = sa.Column(sa.Integer, sa.ForeignKey("tb_colaboradores.id"))
+    solicitante_da_demanda = sa.Column(String(50))
+    # solicitante_da_demanda = sa.Column(Integer, sa.ForeignKey("tb_colaboradores.id"))
 
     def __repr__(self):
-        return f'<Colaboradores {self.nome}>'
+        # return f"<Tarefas(id = {self.id}, titulo_tarefa='{self.titulo_tarefa}'>)"
+        return f"<Tarefas(nome = {self.co_nome_fk}, titulo_tarefa='{self.titulo_tarefa}'>)"
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+class Colaboradores(Base):
+    __tablename__='tb_colaboradores'
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    nome = sa.Column(sa.String(50), index=True)
+    idade = sa.Column(sa.Integer)
+    email = sa.Column(sa.String(40))
+    area_de_atuacao = sa.Column(String(10))
+    tipo_contratacao = sa.Column(String(10))
+    cargo = sa.Column(sa.String(50))
+    cria_tarefa = relationship(
+        Tarefas,
+        primaryjoin= "Colaborador.nome == Tarefas.co_nome_fk",
+        backref= "Tarefas_criadas_pelo_colaborador"
+        
+    )
+    # tarefas_atribuidas = relationship(
+    #     Tarefas,
+    #     primaryjoin= "Colaborador.id == Tarefas.solicitante_da_demanda",
+    #     backref= "Tarefas_atribuidas_pelos_colaborador",
+    # )
+
+    def __repr__(self):
+        return f"<Colaboradores(id={self.id}, nome='{self.nome}')>"
 
     def save(self):
         db_session.add(self)
@@ -32,24 +64,6 @@ class Colaboradores(Base):
         db_session.delete(self)
         db_session.commit()
     
-class Tarefas(Base):
-    __tablename__ = 'tarefas'
-    id = Column(Integer, primary_key=True)
-    tarefa = Column(String(50))
-    colaborador_id = Column(Integer, ForeignKey('colaboradores.id'))
-    colaborador = relationship("Colaboradores")
-
-    def __repr__(self):
-        return f'<Tarefas {self.tarefa}>'
-
-    def save(self):
-        db_session.add(self)
-        db_session.commit()
-
-    def delete(self):
-        db_session.delete(self)
-        db_session.commit()
-
 
 def init_bd():
     Base.metadata.create_all(bind=engine)
